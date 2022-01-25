54db671fdd57391506a4100a5d69a4ea93deba79
SyntaxMA <maciasalvarezruben@elpuig.xeill.net>
2022-01-25 12:45:24 +0100

    Paralelo 1.1 Olivia

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         System.out.println("Esto es una version de prueba");
 
-        System.out.println("Parte Olivia :) ");
-        System.out.println("Parte Juan :D ");
+        System.out.println("Hola soy Oli");
+
     }
 }
